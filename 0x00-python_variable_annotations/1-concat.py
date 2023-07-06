b727f7bd1054fe90b77a25e6b802397e8c2b0710
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2023-07-06 16:18:41 +0000

    made task 100

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-''' Write a type-annotated function concat that takes a string '''
+''' type-annotated function concat that takes a string '''
 
 
 def concat(str1: str, str2: str)-> str:
-    ''' returns a concatenated string '''
+    ''' concatenate string '''
     return str1 + str2

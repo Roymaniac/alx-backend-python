e6fdd13c3e2b07bbd9305b1a07828f6fdbffd157
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2023-07-11 12:16:44 +0000

    basics uses of async/await

---
+++ 0x01-python_async_function/3-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def test(max_delay: int) -> float:
+    '''
+    Task
+    '''
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
+
+asyncio.run(test(5))
